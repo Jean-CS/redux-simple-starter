978296b5aaf7efe031fd3c4829a95fcafc530ae5
Jean Santos <jeansantosc@gmail.com>
2018-08-07 20:36:10 -0300

    update functional component to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,5 +1,12 @@
 import React from 'react'; 
 
-const SearchBar = () => <input />;
+// import React, {Component} from 'react';
+// class SearchBar extends Component {}
+
+class SearchBar extends React.Component {
+  render() {
+    return <input />;
+  }
+}
 
 export default SearchBar;
