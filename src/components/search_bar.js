02d3588c003b790c5373f95ad9a64c671398429c
Jean Santos <jeansantosc@gmail.com>
2018-08-07 21:01:14 -0300

    adding state and displaying data on change of input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,8 +4,25 @@ import React from 'react';
 // class SearchBar extends Component {}
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { query: '' };
+  }
+
   render() {
-    return <input />;
+    return (
+      <div>
+        <input onChange={ event => this.setState({ query: event.target.value }) } />
+        <p>Value of the input: <strong>{this.state.query}</strong></p>
+      </div>
+    );
+
+    // return <input onChange={this.onInputChange} />;
+  }
+
+  // event handler
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
