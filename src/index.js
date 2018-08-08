eed1f11fdef59bb748fac61103a46570dc23e56a
Jean Santos <jeansantosc@gmail.com>
2018-08-07 21:32:18 -0300

    create component to list videos and passing fetched data as props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBu13f0ShYZ-ne_28pblxf_tRwPAq292Dc';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
     return ( 
       <div >
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
